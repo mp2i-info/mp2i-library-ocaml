825c58c0cc610dff59a2b90cb6c13d2d7f3869c2
Quentin Fortier <qpfortier@gmail.com>
2022-03-05 13:19:24 +0100

    Add binary search

---
+++ lib/utils/arrayUtils.ml
@@ -0,0 +1,9 @@
+(** [dichotomy a e] returns true if [a] contains [e], false otherwise. [a] must be a sorted array. *)
+let binary_search a e = 
+  let rec aux i j = (* is e in a.(i), ..., a.(j) ? *)
+      if i > j then false
+      else let m = (i + j)/2 in
+           if a.(m) = e then true
+           else if a.(m) < e then aux (m + 1) j
+           else aux i (m - 1)
+  in aux 0 (Array.length a - 1)
