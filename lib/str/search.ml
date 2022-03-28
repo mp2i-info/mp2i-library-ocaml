b354539c0a1fd7cc808bd225b9a4bd85aac93b9e
Quentin Fortier <qpfortier@gmail.com>
2022-03-28 18:43:34 +0200

    Add rabin_karp

--- lib/str/search.ml
+++ lib/str/search.ml
@@ -4,7 +4,7 @@ let boyer_moore text w =
   let rec make_skip i =
     if i = k - 1 then M.empty
     else make_skip (i+1) |> M.add w.[k - i - 1] i in
-  let skip = make_skip 0 in
+  let skip = make_skip 1 in
 
   let rec search i j = (* test if w[:j] ends in text[i] *)
     if i >= n then -1
@@ -14,3 +14,29 @@ let boyer_moore text w =
       | Some s -> search (max (i + 1) (s + i)) 0
       | None -> search (i + k) 0 in
   search (k - 1) 0
+
+let hash b q s =
+  let rec aux i p =
+    if i = -1 then 0
+    else ((Char.code s.[i])*p + aux (i-1) ((p * b) mod q)) mod q in
+  aux (String.length s - 1) 1
+
+let rec pow a n q = match n with
+  | 0 -> 1
+  | 1 -> a
+  | n -> 
+    let b = pow a (n / 2) q in
+    (b * b * (if n mod 2 = 0 then 1 else a)) mod q
+
+let rabin_karp text w =
+  let k, n = String.length w, String.length text in
+  let q = 3719 in (* prime number for modulo *)
+  let b = 256 in (* number of characters (basis) *)
+  let p = pow b (k - 1) q in (* maximum power of b *)
+  let h_w = hash b q w in
+  let rec search i h =
+    if h = h_w && w = String.sub text i k then i
+    else if i >= n - k then -1
+    else let h_ = (b*(h - p*Char.code text.[i]) + Char.code text.[i + k]) mod q in
+      search (i + 1) (if h_ >= 0 then h_  else h_ + q) in
+  search 0 (hash b q (String.sub text 0 k))
--- test/search_test.ml
+++ test/search_test.ml
@@ -1,9 +1,5 @@
-let%test "search" = Mp2i__Search.(
-  boyer_moore "afo e inform" "nfo" = 7 &&
-  boyer_moore "blabla" "bl" = 0 &&
-  boyer_moore "this is a test" "tt" = -1 &&
-  boyer_moore "this is a test" "this is a test" = 0 &&
-  boyer_moore "this is a test" "test" = 10 &&
-  boyer_moore "ttetestest" "test" = 3 &&
-  boyer_moore "ttetesetes" "test" = -1
-)
+let samples = [("afo e inform", "nfo", 7); ("blabla", "bl", 0); ("this is a test", "tt", -1); ("this is a test", "this is a test", 0); ("this is a test", "test", 10); ("ttetestest", "test", 3); ("ttetesetes", "test", -1)]
+
+let%test "boyer_moore" = List.for_all (fun (t, w, v) -> Mp2i__Search.boyer_moore t w = v) samples;;
+
+let%test "rabin_karp" = List.for_all (fun (t, w, v) -> Mp2i__Search.rabin_karp t w = v) samples;;
